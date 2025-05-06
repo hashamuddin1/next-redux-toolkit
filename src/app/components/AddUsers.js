b42e246f995860645aeb2ab79630f8065cbbeb21
hashamuddin1 <hashamlaptop@gmail.com>
2025-05-06 18:04:50 +0500

    create component

---
+++ src/app/components/AddUsers.js
@@ -0,0 +1,9 @@
+export default function AddUsers() {
+  return (
+    <div>
+      <h1>Add users</h1>
+      <input type="text" placeholder="Add New User"/>
+      <button>Add User</button>
+    </div>
+  );
+}
---
+++ src/app/components/DisplayUsers.js
@@ -0,0 +1,7 @@
+export default function DisplayUsers(){
+    return (
+        <div>
+            <h1>Display users</h1>
+        </div>
+    )
+}
