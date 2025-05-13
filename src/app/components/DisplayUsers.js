ca126601403a965a15b159c7a15d292d6149370f
hashamuddin1 <hashamlaptop@gmail.com>
2025-05-13 18:14:04 +0500

    display data

--- src/app/components/DisplayUsers.js
+++ src/app/components/DisplayUsers.js
@@ -1,7 +1,17 @@
+"use client"
+import { useSelector } from "react-redux"
+
 export default function DisplayUsers(){
+    const userData=useSelector((data)=>data.users)
+    console.log({ userData });
     return (
-        <div>
-            <h1>Display users</h1>
-        </div>
-    )
+      <div>
+        <h1>Display users</h1>
+        <ul>
+          {userData.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
 }
--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,7 +1,6 @@
 import reducer from "./slice";
-
 const { configureStore } = require("@reduxjs/toolkit");
-reducer
-export const store=configureStore({
-    reducer:reducer
-})
+
+export const store = configureStore({
+  reducer
+});
